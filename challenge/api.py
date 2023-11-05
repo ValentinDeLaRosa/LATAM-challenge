2f417386f264ecc58edcd83a89d1fa844c5bb4d3
Valentin de la Rosa <vder@unal.edu.co>
2023-11-05 01:05:38 -0500

    Finished API development

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,35 @@
 import fastapi
+from model import DelayModel
+from pydantic import BaseModel
+import joblib 
+import os
+import pandas as pd
 
 app = fastapi.FastAPI()
+model_path = os.path.join(os.path.dirname(__file__), 'trained_model.pkl')
+trained_model = joblib.load(model_path)
+model = DelayModel()
+
+class PredictionRequest(BaseModel):
+   Fecha-I: object
+   Vlo-I: object
+   Ori-I: object
+   Des-I: object
+   Emp-I: object
+   Fecha-O: object
+   Vlo-O: object
+   Ori-O: object
+   Des-O: object
+   Emp-O: object
+   DIA: int 
+   MES: int 
+   AÑO: int 
+   DIANOM: object
+   TIPOVUELO: object
+   OPERA: object
+   SIGLAORI: object
+   SIGLADES: object
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +38,10 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: PredictionRequest) -> dict:
+    df = pd.DataFrame(data.dict())
+    features = model.preprocess
+    predictions = trained_model.predict(features)
+    return predictions
+
+
--- challenge/model.py
+++ challenge/model.py
@@ -1,51 +1,128 @@
 import pandas as pd
-
+from datetime import datetime
+import numpy as np
 from typing import Tuple, Union, List
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix, classification_report
+import xgboost as xgb
+
 
 class DelayModel:
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01) # Model should be saved in this attribute.
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
-        """
-        Prepare raw data for training or predict.
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]: #Union was written with () but the correct way is to write it with []
+            def get_period_day(date):                
+                date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+                morning_min = datetime.strptime("05:00", '%H:%M').time()
+                morning_max = datetime.strptime("11:59", '%H:%M').time()
+                afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+                afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+                evening_min = datetime.strptime("19:00", '%H:%M').time()
+                evening_max = datetime.strptime("23:59", '%H:%M').time()
+                night_min = datetime.strptime("00:00", '%H:%M').time()
+                night_max = datetime.strptime("4:59", '%H:%M').time()
+                
+                if(date_time > morning_min and date_time < morning_max):
+                    return 'mañana'
+                elif(date_time > afternoon_min and date_time < afternoon_max):
+                    return 'tarde'
+                elif(
+                    (date_time > evening_min and date_time < evening_max) or
+                    (date_time > night_min and date_time < night_max)
+                ):
+                    return 'noche'
+                
+            def is_high_season(fecha):
+                fecha_año = int(fecha.split('-')[0])
+                fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+                range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+                range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+                range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+                range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+                range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+                range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+                range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+                range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+                
+                if ((fecha >= range1_min and fecha <= range1_max) or 
+                    (fecha >= range2_min and fecha <= range2_max) or 
+                    (fecha >= range3_min and fecha <= range3_max) or
+                    (fecha >= range4_min and fecha <= range4_max)):
+                    return 1
+                else:
+                    return 0
 
-        Args:
-            data (pd.DataFrame): raw data.
-            target_column (str, optional): if set, the target is returned.
+            def get_min_diff(data):
+                fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+                fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+                min_diff = ((fecha_o - fecha_i).total_seconds())/60
+                return min_diff
 
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
-            or
-            pd.DataFrame: features.
-        """
-        return
+            data['period_day'] = data['Fecha-I'].apply(get_period_day)
+            data['high_season'] = data['Fecha-I'].apply(is_high_season)
+            data['min_diff'] = data.apply(get_min_diff, axis = 1)
+            threshold_in_minutes = 15
+            data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+
+            features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
+            pd.get_dummies(data['MES'], prefix = 'MES')], 
+            axis = 1
+            )
+
+            target_column = data['delay']
+        # """
+        # Prepare raw data for training or predict.
+
+        # Args:
+        #     data (pd.DataFrame): raw data.
+        #     target_column (str, optional): if set, the target is returned.
+
+        # Returns:
+        #     Tuple[pd.DataFrame, pd.DataFrame]: features and target.
+        #     or
+        #     pd.DataFrame: features.
+        # """
+            return (features, target_column)
+    
 
     def fit(
         self,
         features: pd.DataFrame,
         target: pd.DataFrame
     ) -> None:
-        """
-        Fit model with preprocessed data.
+        x_train, x_test, y_train, y_test = train_test_split(features, target, test_size = 0.33, random_state = 42)
+        self._model.fit(x_train, y_train)
+        
+        from joblib import dump
+        dump(self._model, 'trained_model.pkl')
+        
+        # """
+        # Fit model with preprocessed data.
 
-        Args:
-            features (pd.DataFrame): preprocessed data.
-            target (pd.DataFrame): target.
-        """
-        return
+        # Args:
+        #     features (pd.DataFrame): preprocessed data.
+        #     target (pd.DataFrame): target.
+        # """
+        return self._model
 
     def predict(
         self,
         features: pd.DataFrame
     ) -> List[int]:
+        df = pd.read_csv('../data/data.csv')
+        xgboost_y_preds = self._model.predict(features)
+        xgboost_y_preds = [1 if y_pred > 0.5 else 0 for y_pred in xgboost_y_preds]
         """
         Predict delays for new flights.
 
@@ -55,4 +132,17 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        return xgboost_y_preds
+
+
+#My own test: works
+# import pandas as pd    
+# data = pd.read_csv('c:/Users/Semuret/OneDrive - Universidad Nacional de Colombia/Documentos/GitHub/LATAM-challenge/data/data.csv')
+# myModel = DelayModel()
+# features, target = myModel.preprocess(data)
+# print(target)
+# trainedModel = myModel.fit(features, target)
+# print(trainedModel)
+# prediction = trainedModel.predict(features)
+# print(prediction)
+# print(len(prediction))
